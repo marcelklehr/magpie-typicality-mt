3860d835d626856cf9c940932720739dd2a78bf5
Marcel Klehr <mklehr@gmx.net>
2020-07-11 13:39:27 +0200

    Set experiment live

--- experiment/06_main.js
+++ experiment/06_main.js
@@ -28,14 +28,13 @@ $("document").ready(function() {
         ],
         // Here, you can specify all information for the deployment
         deploy: {
-            experimentID: "INSERT_A_NUMBER",
+            experimentID: "154",
             serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/",
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
-            deployMethod: "debug",
-            contact_email: "YOUREMAIL@wherelifeisgreat.you",
-            prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
+            deployMethod: "directLink",
+            contact_email: "mklehr@uos.de",
         },
         // Here, you can specify how the progress bar should look like
         progress_bar: {
